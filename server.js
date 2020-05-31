0ee808a29ffaa41488c5ff29d2dff0f17736cae7
ga58kam <metodi.manov@tum.de>
2020-05-31 11:47:58 +0300

    add initial backend project stuff

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+//environment variables in dotenv file
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//cors middleware
+app.use(cors());
+//this will allow us to parse json
+app.use(express.json());
+
+//our database uri - something that we'll have to get from mongodb dashboard
+const uri = process.env.ATLAS_URI;
+//start connection
+console.log("Attempting database connection...")
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+//require the routes and use them
+
+/*const usersRouter = require('./routes/users');
+const bookingsRouter = require('./routes/bookings');
+
+app.use('/users', usersRouter);
+app.use('/bookings', bookingsRouter);*/
+
+//server is listening on port...
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
