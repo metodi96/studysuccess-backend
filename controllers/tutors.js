9a04c7613fa0d655277b986b461419ac59b9180b
Julian Ivanov <julian.ivanov@tum.de>
2020-06-10 02:40:58 +0300

    Implement request to add time preferences and filtering request

--- controllers/tutors.js
+++ controllers/tutors.js
@@ -23,3 +23,47 @@ exports.tutors_get_one = (req, res) => {
         .then(user => res.json(user))
         .catch(err => res.status(400).json('Error: ' + err));
 }
+
+exports.tutors_add_time_prefs = (req, res) => {
+    const tutorId = req.params.tutorId;
+    User.findById(tutorId)    
+        .then(User.updateOne({ _id: tutorId},
+             { $addToSet: { timePreferences: { day: req.body.day, startTime: req.body.startTime, endTime: req.body.endTime} } },
+              { new: true })
+              .then(() => res.json("Tutor time preferences updated!"))
+              .catch(err => res.status(400).json('Error: ' + err)))
+                .catch(err => res.status(400).json('Error: ' + err));
+}
+
+exports.tutors_get_time_prefs = (req, res) => {
+    User.findById(req.params.tutorId)
+    .then(tutor => res.json(tutor.timePreferences))
+    .catch(err => res.status(400).json('Error: ' + err));
+}
+
+exports.tutors_get_for_subject = (req, res) => {
+    User.find({
+    hasCertificateOfEnrolment: true,
+    hasGradeExcerpt: true
+    })
+    .populate('subjectsToTakeLessonsIn')
+    .populate('subjectsToTeach')
+    .find({'subjectsToTeach' :  req.params.subjectId})
+    .then(tutor => res.json(tutor))
+    .catch(err => res.status(400).json('Error: ' + err))
+}
+
+exports.tutors_get_filtered = (req, res) => {
+    User.find({
+        hasCertificateOfEnrolment: true,
+        hasGradeExcerpt: true
+        })
+        .populate('subjectsToTakeLessonsIn')
+        .populate('subjectsToTeach')
+        .find({'languages': req.body.language})
+        .where('pricePerHour').lte(req.body.pricePerHour)
+        .then(tutor => res.json(tutor))
+        .catch(err => res.status(400).json('Error: ' + err));
+}
+
+
--- routes/tutors.js
+++ routes/tutors.js
@@ -4,8 +4,36 @@ const TutorsController = require('../controllers/tutors');
 
 //get all tutors
 router.get('/', TutorsController.tutors_get_all);
+router.put('/:tutorId/timePreferences', TutorsController.tutors_add_time_prefs);
+router.get('/:tutorId/timePreferences', TutorsController.tutors_get_time_prefs);
+router.get('/:subjectId', TutorsController.tutors_get_for_subject);
+router.get('/:subjectId/filtered', TutorsController.tutors_get_filtered);
+
+
+/**router.route('/filter').get((req, res) => {
+  User.find({
+    hasCertificateOfEnrolment: true,
+    hasGradeExcerpt: true
+    })
+    .populate('subjectsToTakeLessonsIn')
+    .populate('subjectsToTeach')
+    .find({subjectsToTakeLessonsIn: req.body.subject})
+    .find({languages: req.body.languages})
+    .where('pricePerHour').lte(req.body.pricePerHour);
+});*/
 
 //get a specific tutor
-router.get('/:id', TutorsController.tutors_get_one);
+/**router.get('/:id', TutorsController.tutors_get_one);
+router.route('/:subjectId/:id').get((req, res) => {
+  User.find({
+    hasCertificateOfEnrolment: true,
+    hasGradeExcerpt: true
+    })
+    .populate('subjectsToTakeLessonsIn')
+    .populate('subjectsToTeach')
+    .findById(req.params.id)
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: ' + err));
+}); */
 
 module.exports = router;
