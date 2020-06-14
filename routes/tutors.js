6b9d434d8b0cd81cffa549e12193b4aa9f08b5b1
ga58kam <metodi.manov@tum.de>
2020-06-14 19:04:27 +0300

    make filter tutors routes protected

--- routes/tutors.js
+++ routes/tutors.js
@@ -1,3 +1,4 @@
+const checkAuth = require('../middleware/check-auth');
 //we need the express router and to require the model
 const router = require('express').Router();
 const TutorsController = require('../controllers/tutors');
@@ -7,7 +8,7 @@ router.get('/', TutorsController.tutors_get_all);
 router.put('/:tutorId/timePreferences', TutorsController.tutors_add_time_prefs);
 router.get('/:tutorId/timePreferences', TutorsController.tutors_get_time_prefs);
 router.get('/:subjectId', TutorsController.tutors_get_for_subject);
-router.get('/:subjectId/filtered', TutorsController.tutors_get_filtered);
-router.get('/:subjectId/filtered/:tutorId', TutorsController.tutors_get_one);
+router.get('/:subjectId/filtered', checkAuth, TutorsController.tutors_get_filtered);
+router.get('/:subjectId/filtered/:tutorId', checkAuth, TutorsController.tutors_get_one);
 
 module.exports = router;
